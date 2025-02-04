b3e750e54d13bb212848f556fbeaa6db85931a4b
PaktamXbotz <107012758+PaktamXbotz@users.noreply.github.com>
2025-02-05 04:57:19 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const changeColorButton = document.getElementById('changeColorButton');
-    const heart = document.querySelector('.heart');
+const submitButton = document.getElementById('submitButton');
 
-    changeColorButton.addEventListener('click', () => {
-        const newColor = getRandomColor();
-        heart.style.backgroundColor = newColor;
-        heart.style.setProperty('--before-color', newColor);
-        heart.style.setProperty('--after-color', newColor);
-        changeColorButton.style.backgroundColor = newColor; // Change button color to match heart
-    });
-
-    function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
+submitButton.addEventListener('click', () => {
+  alert('Button clicked!');
 });
